300870d74cdf33f5900f3225edd62f4e8a81d081
Franck Lebourgeois <franck.lebourgeois@gmail.com>
2022-06-15 20:00:55 +0200

    Absolute postion from nav to nav-list, logosize

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@ const burger = document.querySelector(".nav-burger");
 burger.addEventListener("click",function(e){
     console.log("test")
     document.querySelector(".nav-list").classList.toggle("nav-list-burg");
-    // document.querySelector(".nav").classList.add("nav-burg");
-    // document.querySelector(".logo").classList.add("logo-burg");
+    document.querySelector(".logo").classList.toggle("logo-burg");
+    document.querySelector(".logo-img").classList.toggle("logo-img-burg");
+    document.querySelector(".logo-ttl").classList.toggle("logo-ttl-burg");
     document.querySelector(".fa").classList.toggle("fa-bars");
     document.querySelector(".fa").classList.toggle("fa-angle-up");
-    // document.querySelector(".header-container").classList.add('header-container-burg');
+    document.querySelector(".header-container").classList.toggle('header-container-burg');
     const listeNI = document.querySelectorAll(".nav-item");
     for(const l of listeNI){
         l.classList.toggle("nav-item-burg");
