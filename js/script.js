ce4e75d6c858a0dfbffb9e52581ca252acd5f0c0
Franck Lebourgeois <franck.lebourgeois@gmail.com>
2022-06-16 14:08:55 +0200

    Making scroll not possible

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector(".nav-burger");
 burger.addEventListener("click",function(e){
     console.log("test")
+    document.querySelector(".template-dark").classList.toggle("body-burg");
     document.querySelector(".nav-list").classList.toggle("nav-list-burg");
     document.querySelector(".logo").classList.toggle("logo-burg");
     document.querySelector(".logo-img").classList.toggle("logo-img-burg");
@@ -8,16 +9,16 @@ burger.addEventListener("click",function(e){
     document.querySelector(".fa").classList.toggle("fa-bars");
     document.querySelector(".fa").classList.toggle("fa-angle-up");
     document.querySelector(".header-container").classList.toggle('header-container-burg');
+    document.querySelector(".nav").classList.toggle("nav-burg");
+    document.querySelector(".header").classList.toggle("header-burg");
+    const listeNL = document.querySelectorAll(".nav-link");
     const listeNI = document.querySelectorAll(".nav-item");
     for(const l of listeNI){
         l.classList.toggle("nav-item-burg");
     }
-    const listeNL = document.querySelectorAll(".nav-link");
     for(const l of listeNL){
         l.classList.toggle("nav-link-burg");
     }
 
-    document.querySelector(".nav").classList.toggle("nav-burg");
-    document.querySelector(".header").classList.toggle("header-burg");
 
 });
