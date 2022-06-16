fde1916bbccf821a83f9e9d8c64ed683cecdc45b
Franck Lebourgeois <franck.lebourgeois@gmail.com>
2022-06-16 16:33:43 +0200

    transition menu

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
 const burger = document.querySelector(".nav-burger");
 burger.addEventListener("click",function(e){
-    console.log("test")
     document.querySelector(".template-dark").classList.toggle("body-burg");
     document.querySelector(".nav-list").classList.toggle("nav-list-burg");
-    document.querySelector(".logo").classList.toggle("logo-burg");
-    document.querySelector(".logo-img").classList.toggle("logo-img-burg");
-    document.querySelector(".logo-ttl").classList.toggle("logo-ttl-burg");
     document.querySelector(".fa").classList.toggle("fa-bars");
     document.querySelector(".fa").classList.toggle("fa-angle-up");
-    document.querySelector(".header-container").classList.toggle('header-container-burg');
-    document.querySelector(".nav").classList.toggle("nav-burg");
+    document.querySelector(".header-container").classList.add('header-container-burg');
+    document.querySelector(".nav").classList.add("nav-burg");
     document.querySelector(".header").classList.toggle("header-burg");
     const listeNL = document.querySelectorAll(".nav-link");
     const listeNI = document.querySelectorAll(".nav-item");
     for(const l of listeNI){
-        l.classList.toggle("nav-item-burg");
+        l.classList.add("nav-item-burg");
     }
     for(const l of listeNL){
-        l.classList.toggle("nav-link-burg");
+        l.classList.add("nav-link-burg");
+        l.addEventListener("click", function(e){
+            document.querySelector(".nav-list").classList.remove("nav-list-burg");
+            document.querySelector(".template-dark").classList.remove("body-burg");
+            document.querySelector(".fa").classList.toggle("fa-bars");
+            document.querySelector(".fa").classList.toggle("fa-angle-up");
+            document.querySelector(".header").classList.remove("header-burg");
+        });
     }
 
 
