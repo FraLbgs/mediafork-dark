016e5969e9941d710b6e6a1c7516391208925ada
Franck Lebourgeois <franck.lebourgeois@gmail.com>
2022-06-17 13:28:59 +0200

    Creating media query instead of new classes

--- js/script.js
+++ js/script.js
@@ -1,27 +1,17 @@
-const burger = document.querySelector(".nav-burger");
+const burger = document.getElementById("mobile-button");
 burger.addEventListener("click",function(e){
-    document.querySelector(".template-dark").classList.toggle("body-burg");
-    document.querySelector(".nav-list").classList.toggle("nav-list-burg");
+    document.body.classList.toggle("overflow");
+    document.getElementById("main-nav").classList.toggle("display");
     document.querySelector(".fa").classList.toggle("fa-bars");
-    document.querySelector(".fa").classList.toggle("fa-angle-up");
-    document.querySelector(".header-container").classList.add('header-container-burg');
-    document.querySelector(".nav").classList.add("nav-burg");
-    document.querySelector(".header").classList.toggle("header-burg");
-    const listeNL = document.querySelectorAll(".nav-link");
-    const listeNI = document.querySelectorAll(".nav-item");
-    for(const l of listeNI){
-        l.classList.add("nav-item-burg");
-    }
-    for(const l of listeNL){
-        l.classList.add("nav-link-burg");
-        l.addEventListener("click", function(e){
-            document.querySelector(".nav-list").classList.remove("nav-list-burg");
-            document.querySelector(".template-dark").classList.remove("body-burg");
-            document.querySelector(".fa").classList.toggle("fa-bars");
-            document.querySelector(".fa").classList.toggle("fa-angle-up");
-            document.querySelector(".header").classList.remove("header-burg");
-        });
-    }
-
-
+    document.querySelector(".fa").classList.toggle("fa-chevron-up");
 });
+
+const listeNL = document.querySelectorAll(".nav-link");
+for(const l of listeNL){
+    l.addEventListener("click", function(e){
+        document.getElementById("main-nav").classList.toggle("display");
+        document.querySelector(".template-dark").classList.toggle("overflow");
+        document.querySelector(".fa").classList.toggle("fa-bars");
+        document.querySelector(".fa").classList.toggle("fa-chevron-up");
+    });
+}
