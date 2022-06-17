8a345587f29d0bb9cb0c64b056fbdaf0a4b2427c
Franck Lebourgeois <franck.lebourgeois@gmail.com>
2022-06-17 13:31:31 +0200

    function to display the nav

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 const burger = document.getElementById("mobile-button");
-burger.addEventListener("click",function(e){
+burger.addEventListener("click", displayNav);
+
+const listeNL = document.querySelectorAll(".nav-link");
+for(const l of listeNL){
+    l.addEventListener("click", displayNav);
+}
+
+function displayNav(e){
     document.body.classList.toggle("overflow");
     document.getElementById("main-nav").classList.toggle("display");
     document.querySelector(".fa").classList.toggle("fa-bars");
     document.querySelector(".fa").classList.toggle("fa-chevron-up");
-});
-
-const listeNL = document.querySelectorAll(".nav-link");
-for(const l of listeNL){
-    l.addEventListener("click", function(e){
-        document.getElementById("main-nav").classList.toggle("display");
-        document.querySelector(".template-dark").classList.toggle("overflow");
-        document.querySelector(".fa").classList.toggle("fa-bars");
-        document.querySelector(".fa").classList.toggle("fa-chevron-up");
-    });
 }
