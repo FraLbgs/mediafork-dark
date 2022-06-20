8dd9e367aecacd463710620d269cfad4c1c90b7d
Franck Lebourgeois <franck.lebourgeois@gmail.com>
2022-06-20 15:56:24 +0200

    info from html to javascript + fix some CSS

--- js/script.js
+++ js/script.js
@@ -18,19 +18,47 @@ function displayNav(){
 
 // making modal
 
-const mods = document.getElementById("mods");
+// const mods = document.getElementById("mods");
+// const cards = document.querySelectorAll("li .card");
+// const close = document.querySelectorAll(".modal-content");
+
+// for(const c of cards){
+//     c.addEventListener("click", function(e){
+//         document.getElementById(this.dataset.serv).classList.add("visibility");
+//     });
+// }
+
+// for(const c of close){
+//     c.addEventListener("click", function(e){
+//             this.classList.remove("visibility");
+//         });
+// }
+
+const services = document.getElementById("services");
 const cards = document.querySelectorAll("li .card");
-const close = document.querySelectorAll(".modal-content");
+const mods = document.createElement("div");
+mods.classList.add("modal");
+const modalContent = document.createElement("div");
+modalContent.classList.add("modal-content");
+mods.appendChild(modalContent);
 
 for(const c of cards){
     c.addEventListener("click", function(e){
-        document.getElementById(this.dataset.serv).classList.add("visibility");
+        const modal =
+                    '<i class="fa fa-times" aria-hidden="true"></i>'+
+                    '<h3 class="modal-title">'+document.getElementById(this.dataset.ttl).innerText+'</h3>'+
+                    '<img class="modal-icon" src="'+document.getElementById(this.dataset.img).src+'" alt="Web services">'+
+                    '<p class="modal-info">'+this.dataset.info+'</p>';
+        
+        modalContent.innerHTML=modal;
+        console.log(mods);
+        services.appendChild(mods);
+        services.appendChild(document.getElementById("cards"));
+        mods.classList.add("visibility");
+        
     });
 }
 
-for(const c of close){
-    c.addEventListener("click", function(e){
-            this.classList.remove("visibility");
-        });
-}
-
+modalContent.addEventListener("click",function(e){
+    mods.classList.remove("visibility");
+});
